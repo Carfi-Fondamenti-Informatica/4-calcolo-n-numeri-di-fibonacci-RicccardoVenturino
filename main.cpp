abd9d9ea86ec96a62bc8a9b12f3e7a11ca0287a3
RicccardoVenturino <100801309+RicccardoVenturino@users.noreply.github.com>
2022-04-02 20:19:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0, num1 = 1, num2 = 1, somma = 0;
+    cout << "Quanti numeri della serie di Fibonacci devono essere stampati? (almeno 2)" << endl;
+    cin >> n;
+
+    if(n < 2) {
+        cout << "errore" << endl;
+    }else{
+        cout << "1" << endl;
+        cout << "1" << endl;
+        for(int i=0; i<n-2; i++) {
+            somma = num1 + num2;
+            cout << somma << endl;
+            num1 = num2;
+            num2 = somma;
+        }
+    }
+    return 0;
 }
+
